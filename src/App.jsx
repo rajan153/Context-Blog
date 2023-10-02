7a353729f36593bf693ccbd9bb84896fccec7570
Rajan <rajanmasih154@gmail.com>
2023-10-02 23:42:24 +0530

    Added hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,39 @@
 import { useContext, useEffect } from "react";
 import "./App.css";
-import Footer from "./components/Footer";
-import Main from "./components/Main";
 import { blogContext } from "./context/BlogContext";
+import { Route, Routes, useLocation, useSearchParams } from "react-router-dom";
+import Home from "./pages/Home"
+import BlogPage from "./pages/BlogPage"
+import TagPage from "./pages/TagPage"
+import CategoryPage from "./pages/CategoryPage"
 
 function App() {
   const { fetchData } = useContext(blogContext);
 
+  const [searchParams, setSearchParams] = useSearchParams();
+  const location = useLocation();
+
   useEffect(() => {
-    fetchData();
-  }, []);
+    const page = searchParams.get("page") ?? 1;
+    if (location.pathname.includes("tags")) {
+      // Tag wala page show krna hai
+      const tag = location.pathname.split("/").at(-1).replaceAll("-", " ");
+      fetchData(Number(page), tag);
+    } else if (location.pathname.includes("categories")) {
+      const category = location.pathname.split("/").at(-1).replaceAll("-", " ");
+      fetchData(Number(page), null, category);
+    } else {
+      fetchData(Number(page));
+    }
+  }, [location.pathname, location.search]);
 
   return (
-    <div className="flex flex-col items-center justify-center gap-8">
-      <h1 className="w-full bg-slate-400 text-white p-4 text-2xl font-bold text-center">
-        Blog Site
-      </h1>
-      <Main />
-      <Footer />
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/blog/:blogId" element={<BlogPage />} />
+      <Route path="/tags/:tag" element={<TagPage />} />
+      <Route path="/categories/:category" element={<CategoryPage />} />
+    </Routes>
   );
 }
 
